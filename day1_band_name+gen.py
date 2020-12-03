41b5d011d2550cd41e704a2f15701f7dde3e171d
luizaq <37643140+luizaq@users.noreply.github.com>
2020-12-03 02:03:41 -0300

    Create day1_band_name+gen.py
    day one project

---
+++ day1_band_name+gen.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print ("Hello! Welcome to the band name gen \n")
+#2. Ask the user for the city that they grew up in.
+city=input ("Whats the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet=input("whats your pets name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print ("your band name is: " +city+" "+ pet)
